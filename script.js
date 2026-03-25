b63cf6f3f1c6a68a24dceec31ed19d602342c14a
Martin <majamc@outlook.com>
2026-03-25 23:33:14 +0000

    making progress

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
+  //getting acsess to the dom eement i need
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
 
+  // loop for extracting the data and placeing it on the page
+  for (let i = 0; i < episodeList.length; i++) {
+    const { id, name, season, number, summary, image } = episodeList[i];
+    const { medium } = image;
+    //apends a div and a srting inside it
+    const perEpisode = document.createElement("div");
+     rootElem.appendChild(perEpisode);
+    const pic = new Image();
+    pic.src = medium;
+    rootElem.appendChild(pic);
+    const infoAboutEpisode = document.createTextNode(
+      `id:-${id} title:-${name} season:-${season} episode:-${number} ${summary} `,
+    );
+    rootElem.appendChild( infoAboutEpisode);
+    
+  }
+}
 window.onload = setup;
